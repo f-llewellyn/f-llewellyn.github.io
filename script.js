30b76f8891ba97a4589e4466f77191b54679150b
f-llewellyn <llewellynfinn@gmail.com>
2020-05-23 21:40:18 +0100

    Filled content and added resources page.
    Filled out each page with actual information, and added a resources/thanks page with a new button to acces.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function aboutFunc() {
     }, 1000);
     setTimeout(() => {  
         title.innerText = "About";
-        content.innerText = "This will say things about me!";
+        content.innerText = "I'm Finn. I like to think I can write useful and clean programs in Python and Javascript, as well as cool websites with vanilla HTML, CSS and JS.";
     }, 500);
     
 }
@@ -45,7 +45,7 @@ function futureFunc() {
     }, 1000);
     setTimeout(() => {  
         title.innerText = "Plans For The Future"
-        content.innerText = "This will tell you about my plan for the future."
+        content.innerText = "Eventually, I'd like to be a full stack web developer, or program developer. I currently know vanilla HTML, CSS and JavaScript, but I'm currently working on learnin a back-end language (node.js) and some form of database languge (MySQL)."
     }, 500);
 }
 
@@ -59,6 +59,19 @@ function contactFunc() {
     }, 1000);
     setTimeout(() => {  
         title.innerText = "Contact"
-        content.innerText = "This will have my contact details."
+        content.innerText = "Email: llewellynfinn@gmail.com \n Snapchat: fllewell8900"
+    }, 500);
+}
+function resFunc() {
+    title.classList.add("fade")
+    content.classList.add("fade")
+    setTimeout(() => {  
+        title.classList.remove("fade");
+        content.classList.remove("fade");
+        check.checked = false 
+    }, 1000);
+    setTimeout(() => {  
+        title.innerText = "Resources"
+        content.innerText = "Thanks to: \n Traversy Media: CSS only burger bar \n Pexels: background image \n Stack Overflow: Random JS"
     }, 500);
 }
