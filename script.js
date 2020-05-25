c6798a23545d7f6cc4df23e77a55fce67bb46bb5
f-llewellyn <llewellynfinn@gmail.com>
2020-05-25 21:03:37 +0100

    Cleaned up code and commented index.html as well as script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+// Defines all DOM elements
 const title = document.querySelector("h1");
 const content = document.querySelector("p");
 const btn = Array.from(document.querySelectorAll(".btn"));
 const check = document.querySelector("input");
 
-function homeFunc() {
-    console.log(title);
-    title.classList.add("fade")
-    content.classList.add("fade")
+
+// Function that adss and removes the "fade" class in order to create a transition whilst inner html is changing
+function editClass() {
+    // Adds class to elements
+    title.classList.add("fade");
+    content.classList.add("fade");
+    
+    // Waits 500 milliseconds to remove class, and set menu to unchecked, therefore closing the menu
     setTimeout(() => {  
         title.classList.remove("fade");
         content.classList.remove("fade");
         check.checked = false
     }, 1000);
-    setTimeout(() => {  
+}
+
+// Functions that HTML buttons call on
+function homeFunc() {
+    // Calls edit class function defined on line 9
+    editClass();
+    setTimeout(() => {
+        // Changes inner html of elements after 500 milliseconds (when completely faded out)
         title.innerText = "Hello!";
         content.innerText = "Hello, my name is Finn, and this is my website.";
     }, 500);
@@ -21,57 +33,33 @@ function homeFunc() {
 
 function aboutFunc() {
     console.log(title);
-    title.classList.add("fade")
-    content.classList.add("fade")
+    editClass();
     setTimeout(() => {  
-        title.classList.remove("fade");
-        content.classList.remove("fade");
-        check.checked = false
-    }, 1000);
-    setTimeout(() => {  
-        title.innerText = "About";
-        content.innerText = "I'm Finn. I like to think I can write useful and clean programs in Python and Javascript, as well as cool websites with vanilla HTML, CSS and JS.";
+        title.innerHTML = "About";
+        content.innerHTML = "I'm Finn. I like to think I can write useful and clean programs in Python and Javascript, as well as cool websites with vanilla HTML, CSS and JS.";
     }, 500);
     
 }
 
 function futureFunc() {
-    title.classList.add("fade")
-    content.classList.add("fade")
-    setTimeout(() => {  
-        title.classList.remove("fade");
-        content.classList.remove("fade");
-        check.checked = false 
-    }, 1000);
+    editClass();
     setTimeout(() => {  
         title.innerText = "Plans For The Future"
-        content.innerText = "Eventually, I'd like to be a full stack web developer, or program developer. I currently know vanilla HTML, CSS and JavaScript, but I'm currently working on learnin a back-end language (node.js) and some form of database languge (MySQL)."
+        content.innerHTML = "Eventually, I'd like to be a full stack web developer, or program developer. I currently know vanilla HTML, CSS and JavaScript, but I'm currently working on learning a back-end language (node.js) and a query languge (MySQL)."
     }, 500);
 }
 
 function contactFunc() {
-    title.classList.add("fade")
-    content.classList.add("fade")
+    editClass();
     setTimeout(() => {  
-        title.classList.remove("fade");
-        content.classList.remove("fade");
-        check.checked = false 
-    }, 1000);
-    setTimeout(() => {  
-        title.innerText = "Contact"
-        content.innerText = "Email: llewellynfinn@gmail.com \n Snapchat: fllewell8900"
+        title.innerHTML = "Contact"
+        content.innerHTML = "Email: <a href='mailto:llewellynfinn@gmail.com'>llewellynfinn@gmail.com</a> <br> Snapchat: fllewellyn8900"
     }, 500);
 }
 function resFunc() {
-    title.classList.add("fade")
-    content.classList.add("fade")
+    editClass();
     setTimeout(() => {  
-        title.classList.remove("fade");
-        content.classList.remove("fade");
-        check.checked = false 
-    }, 1000);
-    setTimeout(() => {  
-        title.innerText = "Resources"
-        content.innerText = "Thanks to: \n Traversy Media: CSS only burger bar \n Pexels: background image \n Stack Overflow: Random JS"
+        title.innerHTML = "Resources"
+        content.innerHTML = "Thanks to: <br> <a href='https://www.youtube.com/user/TechGuyWeb'>Traversy Media</a>: CSS only burger bar <br> <a href='https://www.pexels.com/'>Pexels</a>: background image <br> <a href='https://stackoverflow.com/'>Stack Overflow</a>: Random JS"
     }, 500);
 }
